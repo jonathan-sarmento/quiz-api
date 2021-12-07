78262e0de715041a4804244a961da151c40ef194
Caio Ferrari <caioferrari0484@gmail.com>
2021-12-07 20:55:14 -0300

    Controllers básicos

---
+++ src/QuizApi/Api/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace QuizApi.Api
+{
+    public class ApiResponse<T>
+    {
+        public string Message { get; set; }
+        public bool Succeed { get; set; }
+        public T Results { get; set; }
+    }
+}
---
+++ src/QuizApi/Controllers/PerguntasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuizApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerguntasController : ApiBaseController
+    {
+        public IActionResult Index() => ApiOk("Bem vindo ao quiz-api!");
+    }
+}
